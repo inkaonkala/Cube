025eda1f6357879328e3e0ece0504b8ad8da5c5d
Inka Niska <iniska@c2r6p1.hive.fi>
2024-11-19 12:13:44 +0200

    norm done

--- src/drawing.c
+++ src/drawing.c
@@ -6,13 +6,13 @@
 /*   By: iniska <iniska@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:16:18 by iniska            #+#    #+#             */
-/*   Updated: 2024/11/19 11:25:35 by iniska           ###   ########.fr       */
+/*   Updated: 2024/11/19 12:11:38 by iniska           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-static mlx_texture_t	*backup_box()
+static mlx_texture_t	*backup_box(void)
 {
 	static mlx_texture_t	*blakkis;
 	int						wid;
@@ -20,7 +20,7 @@ static mlx_texture_t	*backup_box()
 
 	wid = 64;
 	hei = 64;
-	if(!blakkis)
+	if (!blakkis)
 	{
 		blakkis = malloc(sizeof(mlx_texture_t));
 		if (!blakkis)
@@ -28,7 +28,7 @@ static mlx_texture_t	*backup_box()
 		blakkis->width = wid;
 		blakkis->height = hei;
 		blakkis->pixels = malloc(wid * hei * 4);
-		if(!blakkis->pixels)
+		if (!blakkis->pixels)
 		{
 			free(blakkis);
 			return (NULL);
@@ -38,41 +38,54 @@ static mlx_texture_t	*backup_box()
 	return (blakkis);
 }
 
-static mlx_texture_t	*get_pics_for_wall(t_game *game)
+static mlx_texture_t	*texas(t_game *game)
 {
-	mlx_texture_t *tex;
-
-	tex = NULL;
-
-	if (game->hit_door == true)  
-	{
-		check_door(game);
-		if (game->door_state == true)	
-			tex = game->door_open_texture;
-		else
-			tex = game->door_close_texture;
-		game->hit_door = false;
-	}
-	else if (game->rays->wall_flag == true)
+	if (game->rays->wall_flag == true)
 	{
 		if (game->rays->ray_angl > 0 && game->rays->ray_angl < PI)
-			tex = game->no_texture;
+			return (game->no_texture);
 		else
-			tex = game->so_texture;
+			return (game->so_texture);
 	}
 	else
 	{
 		if (game->rays->ray_angl > PI / 2 && game->rays->ray_angl < 3 * PI / 2)
-			tex = game->we_texture;
+			return (game->we_texture);
 		else
-			tex = game->ea_texture;
+			return (game->ea_texture);
+	}
+}
+
+static mlx_texture_t	*get_pics_for_wall(t_game *game)
+{
+	mlx_texture_t	*tex;
+
+	tex = NULL;
+	tex = texas(game);
+	if (game->hit_door == true)
+	{
+		check_door(game);
+		if (game->door_state == true)
+			tex = game->door_open_texture;
+		else
+			tex = game->door_close_texture;
+		game->hit_door = false;
 	}
 	if (!tex)
 		tex = backup_box();
 	return (tex);
 }
 
-void	draw_wall(t_game *game, double bot_pixl, double top_pixl, double wall_hi)
+static int	set_y(mlx_texture_t *pic, double top_px, double wall_hi, double y)
+{
+	y = (top_px - (WIN_HEI / 2) + (wall_hi / 2))
+		* (double)pic->height / wall_hi;
+	if (y < 0)
+		y = 0;
+	return (y);
+}
+
+void	draw_wall(t_game *game, double bot_px, double top_px, double wall_hi)
 {
 	mlx_texture_t	*pic;
 	uint32_t		*pixels;
@@ -80,24 +93,22 @@ void	draw_wall(t_game *game, double bot_pixl, double top_pixl, double wall_hi)
 	double			y;
 	int				pos;
 
+	y = 0.0;
 	pic = get_pics_for_wall(game);
 	if (!pic || wall_hi <= 0)
-		return;
+		return ;
 	pixels = (uint32_t *)pic->pixels;
 	x = get_setof_x(game, pic);
 	if (x < 0 || x >= pic->width)
 		return ;
-	y = (top_pixl - (WIN_HEI / 2) + (wall_hi / 2))
-			* (double)pic->height / wall_hi;
-	if (y < 0)
-		y = 0;
-	while (top_pixl < bot_pixl)
+	y = set_y(pic, top_px, wall_hi, y);
+	while (top_px < bot_px)
 	{
 		pos = (int)y * pic->width + (int)x;
 		if (pos < 0 || pos >= (int)(pic->width * pic->height))
 			return ;
-		mlx_put_pixel(game->canvas, game->rays->i, top_pixl, pixels[pos]);
+		mlx_put_pixel(game->canvas, game->rays->i, top_px, pixels[pos]);
 		y += (double)pic->height / wall_hi;
-		top_pixl++;
+		top_px++;
 	}
 }
